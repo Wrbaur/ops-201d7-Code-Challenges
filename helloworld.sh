a5a6fdd039b436048253289ca615b86dba376e98
Will <wrbaur@gmail.com>
2023-10-24 16:59:00 -0500

    helloworld.sh

---
+++ helloworld.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Script Name:                  Hello World
+# Author:                       Will Baur
+# Date of latest revision:      10/24/2023
+# Purpose:                      To print something to the screen
+
+# Declaration of variables
+
+# Declaration of functions
+
+
+# Main
+echo "Hello World"
+
+
+# End
