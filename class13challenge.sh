5172467baebfe972c1f8e6a4108ebf5126f92e48
Will <wrbaur@gmail.com>
2023-11-08 19:47:30 -0600

    class13challene.sh

--- class13challenge.sh
+++ class13challenge.sh
@@ -8,27 +8,44 @@
 # Requirements
 # Create a script that asks a user to type a domain, then displays information about the typed domain
 
+
+# Function to gather domain information
+gather_domain_info() {
+    domain="$1"
+    
+    # Perform whois lookup
+    whois_result=$(whois "$domain")
+
+    # Perform dig query
+    dig_result=$(dig "$domain")
+
+    # Perform host lookup
+    host_result=$(host "$domain")
+
+    # Perform nslookup
+    nslookup_result=$(nslookup "$domain")
+
+    # Create a timestamp for the filename
+    timestamp=$(date +"%Y%m%d%H%M%S")
+
+    # Output the results to a .txt file
+    output_file="domain_info_$timestamp.txt"
+    echo "=== WHOIS ===" > "$output_file"
+    echo "$whois_result" >> "$output_file"
+    echo "=== DIG ===" >> "$output_file"
+    echo "$dig_result" >> "$output_file"
+    echo "=== HOST ===" >> "$output_file"
+    echo "$host_result" >> "$output_file"
+    echo "=== NSLOOKUP ===" >> "$output_file"
+    echo "$nslookup_result" >> "$output_file"
+
+    # Open the .txt file with the default text editor
+    xdg-open "$output_file"
+}
+
+# Prompt the user to input a domain name
 echo "Enter a domain name:"
-read domain
+read user_input_domain
 
-# Perform whois lookup
-whois_result=$(whois $domain)
-
-# Perform dig query
-dig_result=$(dig $domain)
-
-# Perform host lookup
-host_result=$(host $domain)
-
-# Perform nslookup
-nslookup_result=$(nslookup $domain)
-
-# Display the results
-echo "=== WHOIS ==="
-echo "$whois_result"
-echo "=== DIG ==="
-echo "$dig_result"
-echo "=== HOST ==="
-echo "$host_result"
-echo "=== NSLOOKUP ==="
-echo "$nslookup_result"
+# Call the function to gather and save domain information
+gather_domain_info "$user_input_domain"
